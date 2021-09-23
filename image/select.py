3346f786baeb423ecf498dfa1d307eb36e9110cb
Tkay14041 <takuya@uchida.im>
2021-09-23 20:25:58 +0900

    refactor

---
+++ image/select.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from image.image import Image
+
+
+class Select:
+
+    def __init__(self):
+        self.ok_button = Image('resources/image/select/ok_button.jpg')
+        self.next_button = Image('resources/image/select/next_button.jpg')
+        self.parents_random_select = Image('resources/image/select/parents_random_select.jpg')
+        self.support_random_select = Image('resources/image/select/support_random_select.jpg')
+        self.start_training = Image('resources/image/select/start_training.jpg')
+        self.confirm_starting_training = Image('resources/image/select/confirm_starting_training.jpg')
--- main.py
+++ main.py
@@ -3,75 +3,80 @@ import time
 
 import android_auto_play_opencv as am
 
+from image.image import Image
+from image.images import Images
+from training.training_mode import TrainingMode
+from umamusume.mejiro_mcqueen import MejiroMcQueen
+
 adbpath = ''
 aapo = am.AapoManager(adbpath)
 umamusume_path = 'jp.co.cygames.umamusume/jp.co.cygames.umamusume_activity.UmamusumeActivity'
 
 
-def main():
-    training_mode = './screen_images/select/aoharu_mode.jpg'
-    target_umamusume = './screen_images/uma/mejiro_mcqueen.jpg'
-    start_app()
-    tap_training_button()
-    select_training(training_mode)
-    select_umamusume(target_umamusume)
-    select_random_parents()
-    select_support()
-    start_training()
+class Main:
 
+    def __init__(self):
+        self.training_mode = TrainingMode.AOHARU
+        self.target_umamusume = MejiroMcQueen()
+        self.images = Images()
 
-def touch_image(image: str):
-    touched = False
-    while not touched:
+    def execute(self):
+        self.start_app()
+        self.tap_training_button()
+        self.select_training()
+        self.select_umamusume()
+        self.select_random_parents()
+        self.select_support()
+        self.start_training()
+
+    @staticmethod
+    def touch_image(image: Image):
+        touched = False
+        while not touched:
+            aapo.screencap()
+            if aapo.touchImg(image.str_path):
+                touched = True
+                aapo.sleep(1)
+            else:
+                print(f'image not found: {image.str_path}')
+            time.sleep(1)
+
+    @staticmethod
+    def start_app():
+        aapo.start(umamusume_path)
+        # aapo.sleep(10)
+        print('app has started.')
+
+    def tap_training_button(self):
+        self.touch_image(self.images.home.training_button)
+
+    @staticmethod
+    def is_target_training_mode(training_mode: TrainingMode) -> bool:
         aapo.screencap()
-        if aapo.touchImg(image):
-            touched = True
-            aapo.sleep(1)
-        else:
-            print(f'image not found: {image}')
-        time.sleep(1)
+        return aapo.chkImg2(training_mode.image.str_path)
 
+    def select_training(self):
+        if self.is_target_training_mode(self.training_mode):
+            self.touch_image(self.images.select.next_button)
 
-def start_app():
-    aapo.start(umamusume_path)
-    # aapo.sleep(10)
-    print('app has started.')
+    def select_umamusume(self):
+        self.touch_image(self.target_umamusume.thumbnail)
+        self.touch_image(self.images.select.next_button)
 
+    def select_random_parents(self):
+        self.touch_image(self.images.select.parents_random_select)
+        self.touch_image(self.images.select.ok_button)
+        self.touch_image(self.images.select.next_button)
 
-def tap_training_button():
-    touch_image('./screen_images/home/training_button.jpg')
+    def select_support(self):
+        self.touch_image(self.images.select.support_random_select)
+        self.touch_image(self.images.select.ok_button)
 
-
-def is_target_training_mode(training_mode: str) -> bool:
-    aapo.screencap()
-    return aapo.chkImg2(training_mode)
-
-
-def select_training(training_mode: str):
-    if is_target_training_mode(training_mode):
-        touch_image('./screen_images/select/next_button.jpg')
-
-
-def select_umamusume(target_umamusume: str):
-    touch_image(target_umamusume)
-    touch_image('./screen_images/select/next_button.jpg')
-
-
-def select_random_parents():
-    touch_image('./screen_images/select/parents_random_select.jpg')
-    touch_image('./screen_images/select/ok_button.jpg')
-    touch_image('./screen_images/select/next_button.jpg')
-
-
-def select_support():
-    touch_image('./screen_images/select/support_random_select.jpg')
-    touch_image('./screen_images/select/ok_button.jpg')
-
-
-def start_training():
-    touch_image('./screen_images/select/start_training.jpg')
-    touch_image('./screen_images/select/confirm.jpg')
+    def start_training(self):
+        self.touch_image(self.images.select.start_training)
+        self.touch_image(self.images.select.confirm_starting_training)
 
 
 if __name__ == '__main__':
-    main()
+    main = Main()
+    main.execute()
